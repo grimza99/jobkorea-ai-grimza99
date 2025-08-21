7b343f4d532702120e16b1f0707c1a639eb8b82b
grimza99 <gbtmxlf@naver.com>
2025-08-22 01:40:14 +0900

    feat: LabelCheckInput 컴포넌트 추가 및 스타일 정의

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import LabelCheckInput from "./components/common/LabelCheckInput";
+
 export default function App() {
   return (
     <>
       <p>홈페이지</p>
+      <LabelCheckInput text="테스트" />
     </>
   );
 }
---
+++ src/components/common/LabelCheckInput.tsx
@@ -0,0 +1,50 @@
+import clsx from "clsx";
+import { useState } from "react";
+
+interface LabelCheckInputProps {
+  text: string;
+  className?: string;
+  checked?: boolean;
+  onChange?: (checked: boolean) => void;
+}
+
+export default function LabelCheckInput({
+  text,
+  checked = false,
+  onChange,
+}: LabelCheckInputProps) {
+  const [isChecked, setIsChecked] = useState(checked ?? false);
+
+  const handleClick = () => {
+    setIsChecked((prev) => !prev);
+    onChange?.(isChecked);
+  };
+
+  return (
+    <label
+      className={clsx(
+        `flex items-center justify-center w-fit`,
+        isChecked ? "rounded-primary-small" : "rounded-gray-small"
+      )}
+      data-variant={isChecked ? "rounded-primary-small" : "rounded-gray-small"}
+      data-checked={isChecked}
+    >
+      <input
+        type="checkbox"
+        checked={isChecked}
+        value={isChecked ? "on" : "off"}
+        name="employmentType"
+        onChange={handleClick}
+        className="hidden"
+      />
+      <span
+        className="mx-3 my-2.5"
+        data-variant={
+          isChecked ? "rounded-primary-small" : "rounded-gray-small"
+        }
+      >
+        {text}
+      </span>
+    </label>
+  );
+}
