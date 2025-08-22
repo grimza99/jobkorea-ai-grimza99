696cadcb9604228ef96e99bd8101174edb8e9597
grimza99 <gbtmxlf@naver.com>
2025-08-22 17:46:49 +0900

    feat: App 컴포넌트에 안내 문구 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ export default function App() {
   return (
     <>
       <FilterHeader />
+      <div className="flex w-full items-center justify-center bg-background-gray px-6 py-5">
+        <p className="text-gray400">모든 채용메뉴에 공통 반영됩니다.</p>
+      </div>
     </>
   );
 }
